96b033336310e342f0930fe44353b02845d44fba
Filipe Angelo <filipeangelo.fa@gmail.com>
2023-02-03 16:56:46 -0300

    Update perguntion

---
+++ Quiz App/questions.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        question: "Qual a tradução de HTML?",
+        answers: [
+            { option: "Hyper Text Markup Language", correct: true },
+            { option: "Hiper Text Language Markup", correct: false },
+            { option: "Hipher Language Markup", correct: false },
+        ],
+    },
+    {
+        question: "Para que serve a propriedade < P >?",
+        answers: [
+            { option: "Adicionar uma div", correct: false },
+            { option: "Adicionar um titulo", correct: true },
+            { option: "Adicionar um paragrafo", correct: false },
+        ],
+    },
+    {
+        question: "Quanto é 10 X 8?",
+        answers: [
+            { option: "800", correct: false },
+            { option: "90", correct: false },
+            { option: "80", correct: true },
+        ],
+    },
+    {
+        question: "Quanto é 7 X 7?",
+        answers: [
+            { option: "35", correct: false },
+            { option: "49", correct: true },
+            { option: "14", correct: false },
+        ],
+    },
+    {
+        question: "No CSS, podemos utlizar variáveis para armazenar valores",
+        answers: [
+            { option: "Verdadeiro", correct: false },
+            { option: "Falso", correct: false },
+            { option: "Com ajuda do JS", correct: true },
+        ],
+    },
+]
